0f1767135aa18f93f1384829f2adddd2f2282169
Born.Pt <belorico@hotmail.com>
2024-09-14 20:46:18 +0100

    Create deploy.js

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+  const initialSupply = ethers.utils.parseUnits('1000', 18);  // 1000 tokens with 18 decimals
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const myToken = await MyToken.deploy(initialSupply);
+
+  await myToken.deployed();
+  console.log("MyToken deployed to:", myToken.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
